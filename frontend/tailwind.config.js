da2dc4a2b149f1c84de8fe475374cee3c9101b31
Notaovercat <fdoge@list.ru>
2023-09-27 02:29:29 +0600

    fixed tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,5 +5,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 };
